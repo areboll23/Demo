749f09bbffe1a1ec9c0ab3ed5fb5b03a021ca1c5
Alexandro J Rebollar <80359523+areboll23@users.noreply.github.com>
2021-03-09 16:35:24 -0500

    Revert "Update HelloWorld.cpp"
    This reverts commit 09bad47f28b0ee6a6270f915eef9cf51c03ba60c.

--- UpdateHelloWorld.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-	cout << "\nHello World!\n";
-	cout << "program modified, but able to revert to original version using GitHub Desktop." << endl;
-	return 0;
-}
-
